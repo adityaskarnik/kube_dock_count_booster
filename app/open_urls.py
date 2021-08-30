5f0fb508c8880d789ab7cf83eaa891ba308c1ae7
Aditya Karnik <aditya.s.karnik@gmail.com>
2021-08-30 11:17:27 +0000

    parameters for chrome

--- app/open_urls.py
+++ app/open_urls.py
@@ -16,14 +16,14 @@ chrome_options.add_argument('--log-level=3')
 
 chrome_options.add_argument('--incognito')
 chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument("start-maximized")
 chrome_options.add_argument("enable-automation")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-infobars")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-browser-side-navigation")
-chrome_options.add_argument("--disable-gpu");
+chrome_options.add_argument("--disable-gpu")
 capabilities = {
     'browserName': 'chrome',
     'chromeOptions': {
