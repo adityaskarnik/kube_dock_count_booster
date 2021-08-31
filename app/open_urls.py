cd27c41c8b194d700d08a313e5c438a958423572
Aditya Karnik <aditya.s.karnik@gmail.com>
2021-08-31 11:38:09 +0530

    adding user agent helps

--- app/open_urls.py
+++ app/open_urls.py
@@ -24,6 +24,7 @@ chrome_options.add_argument("--disable-infobars")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-browser-side-navigation")
 chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36")
 capabilities = {
     'browserName': 'chrome',
     'chromeOptions': {
